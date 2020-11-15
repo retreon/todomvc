5e9d13032caa7890c216e06706dcd67c0688d5e9
Jesse Gibson <JesseTheGibson@gmail.com>
2020-11-14 20:57:24 -0800

    Add style defaults
    I'm going to try using CSS variables for this app. I have a suspicion
    they're more powerful than most people realize.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: var(--color-background);
+  color: var(--color-text);
+  font: var(--font);
+  line-height: 1.4rem;
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+`;
+
+const Todos = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  max-width: calc(var(--unit) * 70);
+`;
+
+const Heading = styled.h1`
+  color: var(--color-title);
+  font-weight: 100;
+  font-size: 100px;
+  line-height: normal;
+  text-align: center;
+  margin: var(--unit) 0;
+  text-rendering: optimizeLegibility;
+`;
 
 export default function App() {
-  return <h1>TODO (mvc)</h1>;
+  return (
+    <Container>
+      <Todos>
+        <Heading>todos</Heading>
+      </Todos>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
+
+const ThemeProvider = styled.div`
+  --color-background: #f5f5f5;
+  --color-title: rgba(175, 47, 47, 0.15);
+  --color-text: #4d4d4d;
+  --font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  --unit: 0.5rem;
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
